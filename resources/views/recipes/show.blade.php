b95a51efbc9afd1b704deb51e16bcd718bf5c84e
Bubka <858858+Bubka@users.noreply.github.com>
2018-09-18 12:24:10 +0200

    fix modal not firing on ingredient 'plus' button

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -22,7 +22,7 @@
     @include('partials.alert_success')
 
     <h4>Ingredients
-        <a data-toggle="modal" data-target="#modalNewIngredient" >
+        <a href="#modalNewIngredient" data-toggle="modal" data-target="#modalNewIngredient" >
             <i class="fas fa-plus-square fa-fw"></i>
         </a>
     </h4>
