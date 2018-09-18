ecd22506529c012dae15e96fadffdddeca772eb3
Bubka <858858+Bubka@users.noreply.github.com>
2018-09-18 13:30:06 +0200

    Size tweaking for New recipe form's controls

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -9,20 +9,17 @@
 
     <form method="post" action="{{url('recipe')}}">
         {{ csrf_field() }}
-
         <div class="row">
-            <div class="form-group col-md-4">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" name="name" required>
+            <div class="form-group form-group-lg">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" name="name" required>
             </div>
         </div>
-
         <div class="row">
-            <div class="form-group col-md-6">
-                <button type="submit" class="btn btn-success">Create Recipe</button>
+            <div class="form-group">
+                <button type="submit" class="btn btn-success btn-lg">Create Recipe</button>
             </div>
         </div>
-    
     </form>
 
     @include('partials.suggest_script')
