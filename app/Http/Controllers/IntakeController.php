6f43f98b8a10e1bc77fff93af46d34ee91af6c12
Bubka <858858+Bubka@users.noreply.github.com>
2019-03-29 14:56:44 +0100

    daily kcal target updated to 2500

--- app/Http/Controllers/IntakeController.php
+++ app/Http/Controllers/IntakeController.php
@@ -33,7 +33,7 @@ class IntakeController extends Controller
 
         $intakes = Intake::with('food')->whereDate('ate_on', '=', $days['intakeDate']->toDateString())->get();
 
-        $objective['kcal'] = 2400;
+        $objective['kcal'] = 2500;
         $objective['protein'] = 140;
         $objective['lipid'] = 70;
 
