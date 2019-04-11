7d542ee98345f80bf0a42e6a972eded402a24eee
Bubka <858858+Bubka@users.noreply.github.com>
2019-04-11 12:12:13 +0200

    apple-mobile-web-app-capable set to true

--- resources/views/masterpage.blade.php
+++ resources/views/masterpage.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="robots" content="noindex, nofollow">
-    <meta name="apple-mobile-web-app-capable" content="no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <title>Food</title>
 
